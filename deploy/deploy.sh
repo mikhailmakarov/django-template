1f5794bba60ad5f1442e9fa6b031902a97992a60
Mikhail Makarov <mikhail@makarov.us>
2019-03-31 03:18:31 +0500

    Moved django project files to subfolder named as project

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,5 +3,5 @@ git pull
 pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
-chown -R www-data:www-data .
+chown -R www-data:www-data ./project
 service uwsgi reload
